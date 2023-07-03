12868dd4fcc525fa2a7473c04e7b1fb8a6fc4129
HenriqueDoehler <henriquedoehler1@gmail.com>
2023-07-03 16:14:48 -0300

    fix design - overlay

--- gusk-imports-soon-page/src/layout/overlay.js
+++ gusk-imports-soon-page/src/layout/overlay.js
@@ -1,22 +1,13 @@
 import { useEffect, useState } from "react";
 import styled, { css, keyframes } from "styled-components";
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100vw;
-  height: 100vh;
-  z-index: 100;
-`;
-
 const grow = keyframes`
   0% { transform: scale(0); opacity: 0; }
   100% { transform: scale(1); opacity: 1; }
 `;
 
 const EmBreve = styled.h1`
+  z-index: 100;
   font-size: 3em;
   text-align: center;
   opacity: 0;
@@ -38,9 +29,5 @@ export default function Overlay() {
     return () => clearTimeout(timer);
   }, []);
 
-  return (
-    // <Container>
-    <EmBreve $show={showEmBreve}>Em breve...</EmBreve>
-    // </Container>
-  );
+  return <EmBreve $show={showEmBreve}>Em breve...</EmBreve>;
 }
